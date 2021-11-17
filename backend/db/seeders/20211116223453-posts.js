02541828d3afa0f030a5b783e5fa94df7b75ca61
valeriareynososf <valeriayearup@gmail.com>
2021-11-16 23:31:05 -0800

    saving

--- backend/db/seeders/20211116223453-posts.js
+++ backend/db/seeders/20211116223453-posts.js
@@ -2,15 +2,6 @@
 
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    /**
-     * Add seed commands here.
-     *
-     * Example:
-     * await queryInterface.bulkInsert('People', [{
-     *   name: 'John Doe',
-     *   isBetaMember: false
-     * }], {});
-    */
    return queryInterface.bulkInsert(
      "Posts",
      [
@@ -63,12 +54,6 @@ Space Shuttle Program, we take the opportunity to revisit this famous mission.`,
   },
 
   down: async (queryInterface, Sequelize) => {
-    /**
-     * Add commands to revert seed here.
-     *
-     * Example:
-     * await queryInterface.bulkDelete('People', null, {});
-     */
     return queryInterface.bulkDelete("Posts", null, {});
   }
 };
