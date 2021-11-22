178b741309c1474597b1d058d81faf5b71ec7aa2
valeriareynososf <valeriayearup@gmail.com>
2021-11-21 22:04:05 -0800

    css

--- frontend/src/components/HomePage/PostPage.js
+++ frontend/src/components/HomePage/PostPage.js
@@ -38,53 +38,65 @@ window.location.reload();
 }
 
   return (
-    <div className="postContainer">
+    <div>
       {posts !== null ? (
+          <h2 className="profileTitle" key={posts.id}>{posts.title}</h2>
+    ) : null}
+      <div className="postContainer">
+        {/* {posts !== null ? (
         <div key={posts.id}>
           <h2 className="profileTitle">{posts.title}</h2>
-          <img src={posts.imgUrl} alt="PostImage" className="PostImage" />
-          <p>{posts.details}</p>
-          <img src={posts.instructions} alt="LegoInstructions" />
-        </div>
-      ) : null}
-      <div>
-        <form onSubmit={handleSubmit}>
-          <br />
-          <textarea
-            value={content}
-            required
-            onChange={(e) => setContent(e.target.value)}
-          />
-          <br />
-          <button
-            type="submit"
-            // disabled={errors.length > 0}
-          >
-            Add Comment
-          </button>
-        </form>
-      </div>
-      <div>
-        {comments !== null ? (
-          <div>
-            {Object.values(comments).map((comment) => (
-              <div key={comment.id} className="commentDiv">
-                {comment.content}
-                {id === comment.user_Id ? (
-                  <>
-                    <Link key={comment.id} to={`/comments/${comment.id}`}>
-                      Edit Comment
-                    </Link>
-                  </>
-                ) : null}
-                {id === comment.user_Id ? (
-                  <button onClick={() => deleteBtn(comment.id)}>delete</button>
-                ) : null}
-              </div>
-            ))}
-            <br />
+    <div className="postContainer">
+    ) : null} */}
+        {posts !== null ? (
+          <div key={posts.id}>
+            {/* <h2 className="profileTitle">{posts.title}</h2> */}
+            <img src={posts.imgUrl} alt="PostImage" className="PostImage" />
+            <p>{posts.details}</p>
+            <img src={posts.instructions} alt="LegoInstructions" />
           </div>
         ) : null}
+        <div>
+          <form onSubmit={handleSubmit}>
+            <br />
+            <textarea
+              value={content}
+              required
+              onChange={(e) => setContent(e.target.value)}
+            />
+            <br />
+            <button
+              type="submit"
+              // disabled={errors.length > 0}
+            >
+              Add Comment
+            </button>
+          </form>
+        </div>
+        <div>
+          {comments !== null ? (
+            <div>
+              {Object.values(comments).map((comment) => (
+                <div key={comment.id} className="commentDiv">
+                  {comment.content}
+                  {id === comment.user_Id ? (
+                    <>
+                      <Link key={comment.id} to={`/comments/${comment.id}`}>
+                        Edit Comment
+                      </Link>
+                    </>
+                  ) : null}
+                  {id === comment.user_Id ? (
+                    <button onClick={() => deleteBtn(comment.id)}>
+                      delete
+                    </button>
+                  ) : null}
+                </div>
+              ))}
+              <br />
+            </div>
+          ) : null}
+        </div>
       </div>
     </div>
   );
