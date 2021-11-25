23624a5a8a051e0ecb41e384bec2f121df9393b9
valeriareynososf <valeriayearup@gmail.com>
2021-11-24 17:09:49 -0800

    finished about

--- frontend/src/components/Navigation/About.js
+++ frontend/src/components/Navigation/About.js
@@ -1,14 +1,36 @@
 import "./Navigation.css";
+import legocar from "../../images/legocar.jpeg"
+import legotruck from "../../images/legotruck.jpeg"
+import randomlego from "../../images/randomlego.jpeg"
 
 function About() {
   return (
-  <>
-  <h1 className="aboutTitle">About</h1>
-  <div className="aboutContainer">
-  <h2 className="aboutH2">What is Rebuildable?</h2>
-  <p className="aboutParagraph">Rebuildable is a clone of Rebrickable. You </p>
-  </div>
-  </>
+    <>
+      <h1 className="aboutTitle">About</h1>
+      <div className="aboutContainer">
+        <h2 className="aboutH2">What is Rebuildable?</h2>
+        <p className="aboutParagraph">
+          Rebuildable is a clone of Rebrickable. You can share your Lego
+          creations and choose from the sets submitted by other designers.
+        </p>
+        <div className="picturesAbout">
+          <div>
+            <img src={legotruck} alt="TruckImage" />
+            <p className="legoCap">I HAVE THIS</p>
+          </div>
+          <span className="mathSigns"> + </span>
+          <div>
+            <img src={randomlego} alt="randomLegoImage" />
+            <p className="legoCap">AND A BUNCH OF THESE</p>
+          </div>
+          <span className="mathSigns"> = </span>
+          <div>
+            <img src={legocar} alt="CarImage" />
+            <p className="legoCap">I CAN BUILD THIS, YAY!</p>
+          </div>
+        </div>
+      </div>
+    </>
   );
 }
 
