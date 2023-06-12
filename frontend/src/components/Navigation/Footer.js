b87c42269b292dc791e65428c6a907ae8ed4f4d6
valeriareynososf <valeriayearup@gmail.com>
2023-06-12 14:57:22 -0700

    erased comments

--- frontend/src/components/Navigation/Footer.js
+++ frontend/src/components/Navigation/Footer.js
@@ -28,23 +28,6 @@ function Footer() {
 
   </BottomNavigation>
   </Box>
-    // <div className="footerContainer">
-    //   <a
-    //     href="https://github.com/valeriareynososf"
-    //     target="_blank"
-    //     className="linkedInLink"
-    //   >
-    //     <i className="fab fa-github"></i>
-    //   </a>{" "}
-    //   Valeria Reynoso{" "}
-    //   <a
-    //     href="https://www.linkedin.com/in/valeria-reynoso-b89210149/"
-    //     target="_blank"
-    //     className="linkedInLink"
-    //   >
-    //     <i className="fab fa-linkedin"></i>
-    //   </a>
-    // </div>
   );
 }
 
